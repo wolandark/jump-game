a823ef2f5280c449d3153c8c35ba4d747fce4875
wolandark <isitjuicyisitfresh@tutanota.com>
2023-12-04 00:30:53 +0330

    added tiled files

---
+++ tiled files/Dungeon Tile Set.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Dungeon Tile Set" tilewidth="8" tileheight="8" tilecount="1080" columns="30">
+ <image source="../../Documents/Gb Studio Assests/Assets/GB Assets/Tileset/Dungeon Tile Set/Dungeon Tile Set-sheet-gbcolors-sheet.png" trans="86c06c" width="240" height="288"/>
+ <tile id="124" probability="50"/>
+ <tile id="432" probability="50"/>
+ <tile id="433" probability="50"/>
+</tileset>
